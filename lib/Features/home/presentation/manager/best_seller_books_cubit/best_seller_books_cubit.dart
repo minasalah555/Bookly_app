c3a4d33b1f5444cb315d7f482ecdbb422b4c7550
minasalah555 <salahmina452@gmail.com>
2025-04-12 04:48:29 +0200

    fetch newset books is done

--- lib/Features/home/presentation/manager/best_seller_books_cubit/best_seller_books_cubit.dart
+++ lib/Features/home/presentation/manager/best_seller_books_cubit/best_seller_books_cubit.dart
@@ -1,9 +1,23 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'best_seller_books_state.dart';
 
 class BestSellerBooksCubit extends Cubit<BestSellerBooksState> {
-  BestSellerBooksCubit() : super(BestSellerBooksInitial());
+  BestSellerBooksCubit(this.homeRepo) : super(BestSellerBooksInitial());
+  final HomeRepo homeRepo;
+  Future<void> fetchNewsetBooks() async {
+    emit(BestSellerBooksLoading());
+    var result = await homeRepo.fetchNewsetBooks();
+    result.fold(
+      (failure) {
+        emit(BestSellerBooksFailure(failure.errMessage));
+      },
+      (books) {
+        emit(BestSellerBooksSuccess(books));
+      },
+    );
+  }
 }
