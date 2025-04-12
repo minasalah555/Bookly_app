eb6dd80e3b6b9f8504b8d801303f3ecaeabe180b
minasalah555 <salahmina452@gmail.com>
2025-04-12 04:43:06 +0200

    create best seller books status

---
+++ lib/Features/home/presentation/manager/best_seller_books_cubit/best_seller_books_cubit.dart
@@ -0,0 +1,9 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:equatable/equatable.dart';
+
+part 'best_seller_books_state.dart';
+
+class BestSellerBooksCubit extends Cubit<BestSellerBooksState> {
+  BestSellerBooksCubit() : super(BestSellerBooksInitial());
+}
